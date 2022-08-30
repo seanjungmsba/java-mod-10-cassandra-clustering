afdb8c7b110ddc93c7f4523b44d33cf5c730595b
seanjungmsba <seanjeong1996@gmail.com>
2022-08-30 18:04:49 -0400

    updated RestServiceApplication

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class RestServiceApplication {
-
 	public static void main(String[] args) {
+		System.setProperty("datastax-java-driver.advanced.request-tracker.class", "RequestLogger");
+		System.setProperty("datastax-java-driver.advanced.request-tracker.logs.success.enabled", "true");
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
-
 }
